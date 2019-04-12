b15e92c3317d2745937a32c7584c675b8e7a9ff0
Phillip Mwaniki Nzuli <mwanikiphillip@gmail.com>
2019-04-12 23:21:02 +0300

    added progress bar component

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { Fragment, useState } from 'react';
 import Message from './Message';
+import Progress from './Progress';
 
 const FileUpload = () => {
 
@@ -56,6 +57,7 @@ const FileUpload = () => {
                     <input type="file" className="custom-file-input" id="customFile" onChange={onChange} />
                     <label className="custom-file-label" htmlFor="customFile">{filename}</label>
                 </div>
+                <Progress percentage={uploadPercentage} />
                 <input type="submit" value="Upload" className="btn btn-primary btn-block mt-4"/>
             </form>
             { uploadedFile && <div className="row mt-5">

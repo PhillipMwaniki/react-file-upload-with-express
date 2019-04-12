ee5161f9ea5d36eb8a9bac9b1bad1e5a4aa23f3a
Phillip Mwaniki Nzuli <mwanikiphillip@gmail.com>
2019-04-12 15:17:13 +0300

    created file upload component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import FileUpload from './components/FileUpload';
 
 const App = () => (
-      <div className="App">
-      app
+      <div className="container mt-4">
+      <h4 className="display-4 text-center mb-4">
+        <i className="fab fa-react"></i> React file Upload
+      </h4>
+      <FileUpload />
       </div>
 )
 

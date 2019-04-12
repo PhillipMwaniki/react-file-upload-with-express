cbe425b5f564172d07c854f9da7f14921e755805
Phillip Mwaniki Nzuli <mwanikiphillip@gmail.com>
2019-04-12 15:25:43 +0300

    set up to use hooks

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,12 +1,21 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const FileUpload = () => {
+
+    const [file, setFile] = useState('');
+    const [filename, setFileName] = useState('Choose File');
+
+    const onChange = e => {
+        setFile(e.target.files[0]);
+        setFileName(e.target.files[0].name);
+    };
+    
     return ( 
         <Fragment>
             <form>
                 <div className="custom-file mb-4">
-                    <input type="file" className="custom-file-input" id="customFile" />
-                    <label className="custom-file-label" htmlFor="customFile">Choose file</label>
+                    <input type="file" className="custom-file-input" id="customFile" onChange={onChange} />
+                    <label className="custom-file-label" htmlFor="customFile">{filename}</label>
                 </div>
                 <input type="submit" value="Upload" className="btn btn-primary btn-block mt-4"/>
             </form>
